3772536981f7dbe28376350e319a701a870989e2
Kvriem <kariemg05@gmail.com>
2026-06-17 22:52:39 +0300

    optmized composed (pushed to test on codespace)

---
+++ scripts/init-postgres.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Creates nessie and airflow databases in shared-postgres
+set -e
+
+function create_db() {
+  local db=$1 user=$2 pass=$3
+  echo "→ Creating database '$db' and user '$user'"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER $user WITH PASSWORD '$pass';
+    CREATE DATABASE $db OWNER $user;
+    GRANT ALL PRIVILEGES ON DATABASE $db TO $user;
+EOSQL
+}
+
+create_db nessie  nessie  nessie
+create_db airflow airflow airflow
